60354e93efda9067129593aba460d9c74ecdaa73
Gabriel Periotto <gperiotto@Gabriels-Mac.local>
2017-01-24 19:11:15 +0000

    added number buttons and simple logic

---
+++ calculator3000/calculator3000/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  calculator3000
+//
+//  Created by Gabriel Periotto on 24/01/2017.
+//  Copyright © 2017 Gabriel Periotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //interface builder
+    @IBOutlet weak var labelDisplay: UILabel!
+    
+    
+    var userTyped:Bool = false;
+    
+    
+    //onClick Listener
+    @IBAction func digitPressed(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!;
+        print("digit pressed was = \(digit)");
+        
+        if(userTyped){
+            labelDisplay.text = labelDisplay.text! + "\(digit)";
+        }else{
+            labelDisplay.text = digit;
+            userTyped = true;
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+ 
